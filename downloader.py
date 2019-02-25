2987b49c65dc2bf01372900d45556997b8320209
Tarangok <tarangok@mail.ru>
2019-02-25 18:27:07 +0300

    Udate password

--- downloader.py
+++ downloader.py
@@ -1,3 +1,3 @@
 import os
 
-os.system("wget -i wgetlist.txt -P archives/ --http-user=tarangok@yandex.ru --http-passwd=polosh22rus ")
+os.system("wget -i wgetlist.txt -P archives/ --http-user=tarangok@yandex.ru --http-passwd=GPOtusur19 ")
