96e21c6a70e69687200b062e0ccfb0f794794e12
KrimsN <deni.tim99@yandex.ru>
2019-02-26 23:16:27 +0700

    return to simple

--- downloader.py
+++ downloader.py
@@ -1,6 +1,3 @@
-import os, zipfile
-os.system("wget https://data2.gosat.nies.go.jp/GosatDataArchiveService/usr/download/ProductPage/download?filePath=/data/wget_list/GU/SWIRL2CO2_GU_V02.80.zip -O wget_list.zip --http-user=tarangok@yandex.ru --http-passwd=GPOtusur19 ")
-#tmpzip = zipfile.ZipFile('wget_list')
-#tmpzip.extractall()
-#os.system("rm wget_list.zip")
-#os.system("wget -i wgetlist.txt -P archives/ --http-user=tarangok@yandex.ru --http-passwd=GPOtusur19 ")
+import os
+
+os.system("wget -i wgetlist.txt -P archives/ --http-user=tarangok@yandex.ru --http-passwd=GPOtusur19 ")
