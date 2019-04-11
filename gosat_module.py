ef39ba65a3a36f6fdb246eee2414f38d830ddd73
KrimsN <deni.tim99@yandex.ru>
2019-04-11 10:12:18 +0700

    Convert_test_update

--- gosat_module.py
+++ gosat_module.py
@@ -33,8 +33,29 @@ def FromTar():
 	os.remove("archiveslist.txt")
 
 def Convert():
-	def convert(filename):
-		f = h5py.File("SWIRL2CO2/"+filename, 'r')
+	os.system("rm hdflist.txt")
+	#os.system("rm points.g2s")
+	os.system("ls SWIRL2CO2/ >> hdflist.txt")
+
+	with open("hdflist.txt") as file:
+		array = [row.strip() for row in file]
+
+	countFiles = len(array)
+	j = 1
+	tmplist = []
+	tmplist.append("None")
+	points = dict()
+	dict_str = 'None'
+	for hdfFile in array:
+		os.system("clear || cls")
+		print("Progress: ", end='')
+
+		for one_point in range(0,int((j*100)/countFiles)):
+			print(".", end='')
+
+		print(" %3.1f"% ((j*100)/countFiles), " %  (", j,"/",countFiles, ')')
+		j+=1
+		f = h5py.File("SWIRL2CO2/"+hdfFile, 'r')
 		Data = f['Data']
 		geolocation = Data['geolocation']
 		longitude = geolocation['longitude']
@@ -54,33 +75,27 @@ def Convert():
 		# Списки долготы и ширины
 		longitudeList = list( longitude )									
 		latitudeList = list( latitude )
-
-		points = [ Point( latitudeList[0], longitudeList[0], timeList[0].decode("utf-8"), valueList[0] ) ]				
-		for i in range( 1, numScan ):										
-			points.append( Point(latitudeList[i], longitudeList[i], timeList[i].decode("utf-8"), valueList[i] ) )		
-
 		
-		return points
-
-
-	os.system("rm hdflist.txt")
-	os.system("rm points.g2s")
-	os.system("ls SWIRL2CO2/ >> hdflist.txt")
-
-	with open("hdflist.txt") as file:
-		array = [row.strip() for row in file]
-
-	countFiles = len(array)
-	i = 1
-	g2s = list()
-	for hdfFile in array:
-		os.system("clear || cls")
-		print("Progress: ", "%3.1f"% ((i*100)/countFiles), " %  (", i,"/",countFiles, ')')
-		g2s += convert(hdfFile)
-		i+=1
-
+		
+		print(dict_str)
+		for i in range(1, numScan):
+			if dict_str == timeList[i].decode("utf-8")[0:7]:
+				#print("0:6_1 ", timeList[i].decode("utf-8")[0:7], " str", dict_str, " OK!" )
+				
+				tmplist.append( Point ( longitudeList[i], latitudeList[i], timeList[i].decode("utf-8"), valueList[i] ))
+			else:
+				#print("0:6_1 ", timeList[i].decode("utf-8")[0:6])
+				points[str(dict_str)] = list(tmplist)
+				tmplist.clear()
+				dict_str = timeList[i].decode("utf-8")[0:7]
+				tmplist.append( Point ( longitudeList[i], latitudeList[i], timeList[i].decode("utf-8"), valueList[i] ))
+			
+	FILENAME = "points.g2s" # *.gosa2sur
+	FILENAME_LITE = "pointslite.g2s" # *.gosa2sur
+	with open(FILENAME, "wb") as file:
+		pickle.dump(points, file)
 	date = ''
-	for p in g2s:
+	for p in points:
 		date = p.GetT()[0:7]
 		f = open('' + date + '.json', 'a+')
 		f.write( '{\n"type": "Feature",\n"geometry": ' + str( geoPoint( ( float( p.GetY() ), float( p.GetX() ) )) ) + ',\n"properties": {\n"value":'+str(p.GetV())+'}\n},\n')
